6908d52a382391ac2f881d2d2caaf4d8e9cecd6a
Roberto Rodrigues Junior <robertogyn19@gmail.com>
2014-09-18 07:23:56 -0300

    Change version to v0.0.0

--- public/js/services.js
+++ public/js/services.js
@@ -5,5 +5,5 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('gogeo-devguide.services', []).
-  value('version', '0.1.0')
+  value('version', '0.0.0')
   ;
